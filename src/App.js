85e42d224b4b06c20ecb3be10cdec4b998842b8d
davisllv <davisllvsnt57@gmail.com>
2023-12-11 21:28:21 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Inicio from "./Paginas/Inicio";
+import SobreMim from "./Paginas/SobreMim";
+
+const { pathname } = window.location;
+
+const initialRender = pathname === "/" ? <Inicio /> : <SobreMim />;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return initialRender;
 }
 
 export default App;
---
+++ src/Paginas/Inicio/index.js
@@ -0,0 +1,5 @@
+const Inicio = () => {
+  return <h1>Olá Mundo!</h1>;
+};
+
+export default Inicio;
