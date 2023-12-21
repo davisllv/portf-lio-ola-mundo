71fa39bbb4c7917bc50ccd626a24a00f4a95f1d8
Davi da Silva dos Santos <davi.santos@scoreplan.com.br>
2023-12-20 22:12:39 -0300

    created the post

--- src/Paginas/Post/index.js
+++ src/Paginas/Post/index.js
@@ -1,19 +1,23 @@
 import { useParams } from "react-router-dom";
+import "./post.css"
 import Posts from "json/db.json";
 import PostModelo from "componentes/PostModelo";
+import ReactMarkdown from "react-markdown";
+
 const Post = () => {
   const { id } = useParams();
   console.log(Posts);
 
   const Post = Posts.find((post) => post.id === Number(id));
 
-  console.log(Post);
   return (
     <PostModelo
       fotoCapa={`/assets/posts/${Post.id}/capa.png`}
       titulo={Post.titulo}
     >
-      {Post.texto}
+      <div className="post-markdown-container">
+        <ReactMarkdown>{Post.texto}</ReactMarkdown>
+      </div>
     </PostModelo>
   );
 };
